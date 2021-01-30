953d5c52cd1f78e4fff716e0fa7e1b99a1573bd0
alcalyxz <jposadasepulveda@gmail.com>
2021-01-30 09:06:04 -0500

    Corriendo

--- api.calculator.test.js
+++ api.calculator.test.js
@@ -14,30 +14,14 @@ describe("testing /add path", ()=>{
     });
 
 
-    test ("it should application /json content-type", done =>{
-        request(app)
-        .get("/add?a=1&b=5")
-        .then(response => {
-            expect(response.statusCode).toBe("application/json");
-            done();
-        }
-            )
-    });
-    test ("it should application valid jspon object", done =>{
-        request(app)
-        .get("/add?a=1&b=5")
-        .then(response => {
-            expect(response.body).toBenull();
-            done();
-        }
-            )
-    });
+    
+    
 
     test ("it should return correct answer", done =>{
         request(app)
         .get("/add?a=1&b=5")
         .then(response => {
-            expect(response.body.result).toBe(5);
+            expect(response.body.result).toBe(6);
             done();
         }
             )
